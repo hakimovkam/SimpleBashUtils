94b78d399ea232357585593cc73be2b13944a766
Rene Tilda <renetild@va-e5.kzn.21-school.ru>
2022-10-30 18:51:20 +0300

    0.1.2 CAT clean

--- src/cat/s21_cat.c
+++ src/cat/s21_cat.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <getopt.h>
 
-#define shortFlags "benstvTE+"
+#define shortFlags "+benstvTE"
 //MARK: - struct flags
     /// это нужно будет закинуть в хэдер
 
@@ -19,7 +19,7 @@ typedef struct options {
 //MARK: - functions
 
 void fileCopy(FILE *, FILE *);
-void parser(int argc, char *argv[], opt *options);
+int parser(int argc, char *argv[], opt *exemplarOpt);
 void numberNonblank();
 
 //MARK: - main
@@ -39,8 +39,9 @@ int main(int argc, char *argv[]) {
             }
         }
     } else {
-        opt options = {0}; // сразу зануляем все значения в структуре options
-        parser(argc, argv, &options);
+        opt exemplarOpt = {0}; // сразу зануляем все значения в структуре exemplarOpt
+        int countOfFlags = parser(argc, argv, &exemplarOpt);
+        printf("%d", countOfFlags);
     }
     return 0;
 }
@@ -55,9 +56,10 @@ int c;
 
 //MARK: - parser
 ///         тут происходит считывание флагов и заполнение экземпляра структуры opt
-void parser(int argc, char *argv[], opt *options) {
-    int longIndex = 0; //указатель на переменную, в которую будет помещен индекс текущего параметра из массива longOption.
-    char opt1;
+int parser(int argc, char *argv[], opt *exemplarOpt) {
+    
+    int longIndex = 0, countOfFlags = 0; //указатель на переменную, в которую будет помещен индекс текущего параметра из массива longOption.
+    char opt;
     
     static struct option longOption[] = {
         {"number-nonblank", 0, NULL, 'b'},
@@ -65,39 +67,45 @@ void parser(int argc, char *argv[], opt *options) {
         {"squeeze-blank", 0, NULL, 's'},
         { NULL, 0, NULL, 0}
     };
-    /* выше создан массив экземпляров структур, которые описаны в библиотеке getopt.h
     
-     struct option {
+    /* выше создан массив экземпляров структур, которые описаны в библиотеке getopt.h
+         struct option {
          const char *name;
          int has_arg;
          int *flag;
          int val;
      }; */
     
-    while ((opt1 = getopt_long(argc, argv, shortFlags, longOption, &longIndex)) != -1) {
-        switch(opt1) {
+    while ((opt = getopt_long(argc, argv, shortFlags, longOption, &longIndex)) != -1) {
+        switch(opt) {
             case 'b':
-                options -> b = 1;
+                exemplarOpt -> b = 1;
+                countOfFlags +=1;
                 break;
             case 'e':
-                options -> e = 1;
-                options -> v = 1;
+                exemplarOpt -> e = 1;
+                exemplarOpt -> v = 1;
+                countOfFlags +=1;
                 break;
             case 'n':
-                options -> n = 1;
+                exemplarOpt -> n = 1;
+                countOfFlags +=1;
                 break;
             case 's':
-                options -> s = 1;
+                exemplarOpt -> s = 1;
+                countOfFlags +=1;
                 break;
             case 't':
-                options -> t = 1;
-                options -> v = 1;
+                exemplarOpt -> t = 1;
+                exemplarOpt -> v = 1;
+                countOfFlags +=1;
                 break;
             default:
                 fprintf(stderr, "use correct flags\n");
                 exit(1);
         }
     }
+    return countOfFlags;
 }
 
 //MARK: - realization of flags
