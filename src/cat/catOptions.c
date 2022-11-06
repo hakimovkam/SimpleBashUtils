3c1e692f5148ff1c69e39789cd4b0cc8d75616e2
Камиль Хакимов <kamilhakimov@MacBook-Pro-Kamil.local>
2022-11-06 12:00:25 +0300

    0.2.1 CAT

--- src/cat/catOptions.c
+++ src/cat/catOptions.c
@@ -1,71 +1,70 @@
 #include "catOptions.h"
 
 void reader(FILE *inputFile, FILE *outputFile) {
-    
-int ch;
-    while ((ch = getc(inputFile)) != EOF)
-        putc(ch, outputFile);
+  int ch;
+  while ((ch = getc(inputFile)) != EOF) putc(ch, outputFile);
 }
 
-void squeezeBlank(opt exemplarOpt, int *flagForSqueezeBlank, char currentChar, char previousChar, int *enterCounter) {
-    if (exemplarOpt.s == 1) {
-        if (currentChar == '\n' && previousChar == '\n') {
-            *enterCounter += 1;
-            *flagForSqueezeBlank = 1;
-        } else {
-            *enterCounter = 0;
-        }
+void squeezeBlank(opt exemplarOpt, int *flagForSqueezeBlank, char currentChar,
+                  char previousChar, int *enterCounter) {
+  if (exemplarOpt.s == 1) {
+    if (currentChar == '\n' && previousChar == '\n') {
+      *enterCounter += 1;
+      *flagForSqueezeBlank = 1;
+    } else {
+      *enterCounter = 0;
     }
+  }
 }
 
-void numberNonblank(opt exemplarOpt, char currentChar, char previousChar, int *lineCounter) {
-    if (exemplarOpt.b == 1) {
-        if (previousChar == '\n' && currentChar != '\n') {
-            *lineCounter += 1;
-            printf("%6d\t", *lineCounter);
-        }
+void numberNonblank(opt exemplarOpt, char currentChar, char previousChar,
+                    int *lineCounter) {
+  if (exemplarOpt.b == 1) {
+    if (previousChar == '\n' && currentChar != '\n') {
+      *lineCounter += 1;
+      printf("%6d\t", *lineCounter);
     }
+  }
 }
 
 void number(opt exemplarOpt, int *lineCounter, char previousChar) {
-    if (exemplarOpt.n == 1 && exemplarOpt.b == 0) {
-        if (previousChar == '\n') {
-            *lineCounter += 1;
-            printf("%6d\t", *lineCounter);
-        }
+  if (exemplarOpt.n == 1 && exemplarOpt.b == 0) {
+    if (previousChar == '\n') {
+      *lineCounter += 1;
+      printf("%6d\t", *lineCounter);
     }
+  }
 }
 
 void flagE(opt exemplarOpt, char currentChar) {
-    if (exemplarOpt.e == 1) {
-        if (currentChar == '\n') {
-            printf("$");
-        }
+  if (exemplarOpt.e == 1) {
+    if (currentChar == '\n') {
+      printf("$");
     }
+  }
 }
 
-int flagV(opt exemplarOpt,char *currentChar) {
-    int flagResult = 0;
-    if (exemplarOpt.v == 1) {
-        if (*currentChar < 32 && *currentChar != '\n' && *currentChar != '\t') {
-            printf("^%c", *currentChar + 64);
-            flagResult = 1;
-        } else if (*currentChar == 127) {
-            printf("^?");
-            flagResult = 1;
-        }
+int flagV(opt exemplarOpt, char *currentChar) {
+  int flagResult = 0;
+  if (exemplarOpt.v == 1) {
+    if (*currentChar < 32 && *currentChar != '\n' && *currentChar != '\t') {
+      printf("^%c", *currentChar + 64);
+      flagResult = 1;
+    } else if (*currentChar == 127) {
+      printf("^?");
+      flagResult = 1;
     }
-    return flagResult;
+  }
+  return flagResult;
 }
 
 int flagT(opt exemplarOpt, char *currentChar) {
-    int flagResult = 0;
-    if (exemplarOpt.t == 1) {
-        if (*currentChar == '\t') {
-            printf("^I");
-            flagResult = 1;
-        }
+  int flagResult = 0;
+  if (exemplarOpt.t == 1) {
+    if (*currentChar == '\t') {
+      printf("^I");
+      flagResult = 1;
     }
-    return flagResult;
+  }
+  return flagResult;
 }
-
--- src/cat/catOptions.h
+++ src/cat/catOptions.h
@@ -1,36 +1,36 @@
 #ifndef SRC_CAT_CATOPTIONS_H
 #define SRC_CAT_CATOPTIONS_H
+#include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <getopt.h>
 
 #define shortFlags "+benstvTE"
 
 typedef struct options {
-    int b;
-    int e;
-    int n;
-    int s;
-    int t;
-    int v;
+  int b;
+  int e;
+  int n;
+  int s;
+  int t;
+  int v;
 } opt;
 
-static struct option longOption[] = {
-    {"number-nonblank", 0, NULL, 'b'},
-    {"number", 0, NULL, 'n'},
-    {"squeeze-blank", 0, NULL, 's'},
-    { NULL, 0, NULL, 0}
-};
+static struct option longOption[] = {{"number-nonblank", 0, NULL, 'b'},
+                                     {"number", 0, NULL, 'n'},
+                                     {"squeeze-blank", 0, NULL, 's'},
+                                     {NULL, 0, NULL, 0}};
 
 void reader(FILE *inputFile, FILE *outputFile);
 void formater(opt exemplarOpt, FILE *inputFile);
 void parser(int argc, char *argv[], opt *exemplarOpt);
-void squeezeBlank(opt exemplarOpt, int *flagForSqueezeBlank, char currentChar, char previousChar, int *enterCounter);
-void numberNonblank(opt exemplarOpt, char currentChar, char previousChar, int *lineCounter);
+void squeezeBlank(opt exemplarOpt, int *flagForSqueezeBlank, char currentChar,
+                  char previousChar, int *enterCounter);
+void numberNonblank(opt exemplarOpt, char currentChar, char previousChar,
+                    int *lineCounter);
 void number(opt exemplarOpt, int *lineCounter, char previousChar);
 void flagE(opt exemplarOpt, char currentChar);
-int flagV(opt exemplarOpt,char *currentChar);
+int flagV(opt exemplarOpt, char *currentChar);
 int flagT(opt exemplarOpt, char *currentChar);
 
-#endif // SRC_CAT_CATOPTIONS_H
+#endif  // SRC_CAT_CATOPTIONS_H
